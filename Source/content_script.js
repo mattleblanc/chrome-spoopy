455d2dba7ff8585f589f85ed1a2e5a30d3137427
mleblanc <matt.leblanc@cern.ch>
2015-10-16 13:53:49 +0200

    Initial commit: editing cloud2butt and creating chrome-spoopy

--- Source/content_script.js
+++ Source/content_script.js
@@ -2,28 +2,34 @@ walk(document.body);
 
 function walk(node) 
 {
-	// I stole this function from here:
+	// I stole this function from someone who stole this function from here:
 	// http://is.gd/mwZp7E
 	
-	var child, next;
+	// MLB added the check for the date, because it's only spooky if it's in spooktober ...
 
-	switch ( node.nodeType )  
+	var d = new Date();
+	if(d.getMonth()==9) // 9 is spooktober
 	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+		var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
+		switch ( node.nodeType )
+		{
+			case 1:  // Element
+			case 9:  // Document
+			case 11: // Document fragment
+				child = node.firstChild;
+				while ( child ) 
+				{
+					next = child.nextSibling;
+					walk(child);
+					child = next;
+				}
+				break;
+
+			case 3: // Text node
+				handleText(node);
+				break;
+		}
 	}
 }
 
@@ -31,12 +37,13 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bSpooky\b/g, "Spoopy");
+	v = v.replace(/\bspooky\b/g, "spoopy");
+	v = v.replace(/\b.*Halloween\b/g, "Spookleoween");
+	v = v.replace(/\b.*halloween\b/g, "spookleoween");
+	v = v.replace(/\bAllhallowe\'en\b/g, "Allspookleowe\'en");
+	v = v.replace(/\ballhallowe\'en\b/g, "allspookleowe\'en");
+	v = v.replace(/\bHallows\b/g, "Spookles");
+	v = v.replace(/\bhallows\b/g, "spookles");
 	textNode.nodeValue = v;
 }
-
-
